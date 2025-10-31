bbba4914f1433d7032149c539b9f190f47e67997
Vinicius Santos <vfssantos@outlook.com>
2025-10-30 22:07:48 -0300

    fixing table startup issue

--- database/index.ts
+++ database/index.ts
@@ -103,7 +103,6 @@ export async function createDatabase(config?: DatabaseConfig): Promise<CopilotzD
 
   const url = config?.url || Deno.env.get("DATABASE_URL") || ":memory:";
 
-  console.log('Migrations: ', [...config?.extended?.migrations || [], ...splitSQLStatements(migrations)])
   const finalConfig: DatabaseConfig = {
     url,
     syncUrl: config?.syncUrl || Deno.env.get("SYNC_DATABASE_URL"),
